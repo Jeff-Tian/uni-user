b0f5244f8f826923376fa9ae11aaccfdd526d0b1
Jeff Tian <jeff.tian@outlook.com>
2020-02-02 17:58:32 +0800

    fix: login button function

--- src/components/login-button.tsx
+++ src/components/login-button.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { Button } from 'antd';
 
+const gotoLoginPage = () => {
+    const w = window.parent ?? window;
+
+    const returnUrl = w.location.href;
+
+    const ssoUrl = `https://sso.pa-ca.me/app/login?r=${encodeURIComponent(returnUrl)}`
+
+    w.location.href = ssoUrl;
+}
+
 export const LoginButton = () => {
-    return <Button>登录</Button>
+    return <Button onClick={gotoLoginPage}>登录</Button>
 }
