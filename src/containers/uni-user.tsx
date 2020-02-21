13393f01e6460c9e8bfde2dad6c0aba6810462e4
Jeff Tian <jeff.tian@outlook.com>
2020-02-21 20:51:19 +0800

    fix: use tarojs/redux

--- src/containers/uni-user.tsx
+++ src/containers/uni-user.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Provider } from "react-redux";
+import { Provider } from "@tarojs/redux";
 import configStore from '../redux/store';
 
 console.log('configuring store...')
