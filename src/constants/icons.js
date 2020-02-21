d6653e7073f20a63cffe10bea3d33f5da7653138
Jeff Tian <jeff.tian@outlook.com>
2020-02-21 15:38:34 +0800

    fix: only keep a little set of antdesign icons

---
+++ src/constants/icons.js
@@ -0,0 +1,3 @@
+export {
+    default as BarChartOutline
+} from "@ant-design/icons/lib/outline/BarChartOutline";
--- webpack/resolve.js
+++ webpack/resolve.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
   alias: {
-    components: path.resolve(__dirname, 'src/components'),
+    components: path.resolve(__dirname, '../src/components'),
+    "@ant-design/icons/lib/dist$": path.resolve(__dirname, "../src/constants/icons.js")
   }
 };
