0a1d3f49520ad07f662463ee37fd542f674fe79a
Jeff Tian <jeff.tian@outlook.com>
2020-02-21 19:57:54 +0800

    fix: upgrade react-redux

--- src/containers/uni-user.tsx
+++ src/containers/uni-user.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Provider } from "react-redux";
 import configStore from '../redux/store';
 
+console.log('configuring store...')
 const store = configStore()
+console.log('store configured.')
 
 const UniUserContainer = ({ children }) => <Provider store={store}>{children}</Provider>
 
