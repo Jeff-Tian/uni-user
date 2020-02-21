ab6cfd5a20f13aaae6ff19b4b95108e68be13894
Jeff Tian <jeff.tian@outlook.com>
2020-02-21 17:54:50 +0800

    feat: export UniUserContainer, LoginButton, LoginStatus

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import UniUserContainer from './containers/uni-user';
+
+
+export { UniUserContainer };
+export { LoginButton } from './components/login-button';
+export { LoginStatus } from './components/login-status';
+
 export { UniUser } from './user';
